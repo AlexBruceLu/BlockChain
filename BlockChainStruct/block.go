3187b89f59939eecd31300d2e6cb8dbdc86d74c7
AlexBruceLu <lu_100_yu@163.com>
2018-10-14 23:22:58 +0800

    完整区块信息，添加hash

--- BlockChainStruct/block.go
+++ BlockChainStruct/block.go
@@ -1,18 +1,60 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"time"
+)
+
 //定义区块属性
 type Block struct {
-	PrevHash []byte //前区块哈希值
-	Hash []byte //当前哈希值
-	Data []byte //所要传递数据
+	Version    uint64 //版本号
+	PrevHash   []byte //前区块哈希值
+	MerKelRoot []byte //梅歇尔根
+	TimeStamp  uint64 //时间戳
+	Difficulty uint64 //难度值
+	Nonce      uint64 //随机数，挖矿要找的数
+	Hash       []byte //当前哈希值
+	Data       []byte //所要传递数据
 }
 
 //创建区块
-func NewBlock(prevHash,data []byte) *Block {
-	block:=Block{
-		PrevHash:prevHash,
-		Hash:[]byte{},
-		Data:[]byte(data),
+func NewBlock(prevHash, data []byte) *Block {
+	block := Block{
+		Version:    00,
+		PrevHash:   prevHash,
+		MerKelRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficulty: 0, //写的随机无效值
+		Nonce:      0, //写的随机无效值
+		Hash:       []byte{},
+		Data:       []byte(data),
 	}
 	return &block
+}
+
+//实现uint64转字符切片的辅助函数
+func Uint64ToByte(num uint64) []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	if err != nil {
+		panic(err)
+	}
+	return buffer.Bytes()
+}
+
+//生成哈希、
+func (block *Block) SetHash() {
+	var blockInfo []byte
+	blockInfo = append(blockInfo, block.Hash...)
+	blockInfo = append(blockInfo, block.PrevHash...)
+	blockInfo = append(blockInfo, block.Data...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Difficulty)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Nonce)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Version)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.TimeStamp)...)
+	blockInfo = append(blockInfo, block.MerKelRoot...)
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:] //定长数组转切片
 }
--- BlockChainStruct/main.go
+++ BlockChainStruct/main.go
@@ -6,6 +6,7 @@ func main() {
 	prevHash:=[]byte("")
 	data:=[]byte("小明向小红转了5枚比特币")
 	bc:=NewBlock(prevHash,data)
+	fmt.Printf("当前区块高度%d\n",bc.Version)
 	fmt.Printf("当前区块前区块哈希值%v\n",bc.PrevHash)
 	fmt.Printf("当前区块所传递信息%s\n",bc.Data)
 	fmt.Printf("当前区块哈希值%v\n",bc.Hash)
