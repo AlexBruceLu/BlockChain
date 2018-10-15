6f6b1233fe01519d209ec3d22e50e393766993ec
AlexBruceLu <lu_100_yu@163.com>
2018-10-15 18:56:58 +0800

    version 1 创建区块结构，实现区块链

--- BlockChainStruct/main.go
+++ BlockChainStruct/main.go
@@ -8,7 +8,7 @@ func main() {
 	bc.AddBlocks("小明向小红转了5枚比特币")
 	for i, v := range bc.Blocks {
 		fmt.Printf("----------当前区块高度%d----------\n", i)
-		fmt.Printf("当前区块前区块哈希值%x\n",v.PrevHash)
+		fmt.Printf("当前区块前区块哈希值%x\n", v.PrevHash)
 		fmt.Printf("当前区块所传递信息%s\n", v.Data)
 		fmt.Printf("当前区块哈希值%x\n", v.Hash)
 	}
